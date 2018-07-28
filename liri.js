a5d2e83ad47c6c12fd8a11f37b7b7e7f23b806e1
vsaraogi <mailvsaraogi@gmail.com>
2018-07-28 02:33:30 -0400

    Now taking all the input commands

--- liri.js
+++ liri.js
@@ -11,6 +11,10 @@ var fs = require("fs");
 var clientSpotify = new Spotify(keys.spotify);
 var clientTwitter = new Twitter(keys.twitter);
 
+function isUndefined(param) {
+    return (typeof param === "undefined");
+};
+
 function getTwitter() {
     var params = { screen_name: 'JohnSmi23366922', count: 20 };
     clientTwitter.get('statuses/user_timeline', params, function (error, tweets, response) {
@@ -25,8 +29,13 @@ function getTwitter() {
     });
 }
 
-function getSpotify() {
-    clientSpotify.search({ type: 'track', query: 'Baby one more time', limit: 2 }, function (err, data) {
+function getSpotify(songTitle) {
+
+    if ( isUndefined(songTitle) ) {
+        songTitle = "The Sign";
+    }
+
+    clientSpotify.search({ type: 'track', query: songTitle, limit: 1 }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
@@ -34,27 +43,88 @@ function getSpotify() {
         // console.log(JSON.stringify(data, null, 2));
         let items = data.tracks.items;
         items.forEach(elem => {
-            console.log(elem);
+            // console.log(elem);
             // console.log(JSON.stringify(elem, null, 2));
         })
-        // console.log(data);
+        console.log("***DEBUG***: songs count:", items.length);
+        let song = items[0];
+        let artists = song.artists;
+        let songArtists;
+        artists.forEach(elem => {
+            songArtists = (songArtists ? songArtists : "") + "\"" + elem.name + "\" ";
+        })
+        console.log("Artist(s)", songArtists);
+        console.log("Song name: ", song.name);
+        let link = song.preview_url;
+        if (link === null) {
+            link = song.external_urls.spotify;
+        }
+        console.log("Preview link: ", link);
+        console.log("Album: ", song.album.name);
+        // console.log(JSON.stringify(song));
     })
 }
 
 function getOmdb(movieTitle) {
     const apiKey = 80665998;
 
-    movieTitle
+    if ( isUndefined(movieTitle) ) {
+        movieTitle = "Mr. Nobody";
+    }
+
     let url = "http://www.omdbapi.com/?apikey=" + apiKey + "&t=" + movieTitle.replace(/\s+/g, "+");
     console.log(url);
 
     request(url, function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log(response);
-        // console.log('body:', body); // Print the HTML for the Google homepage.
+        // console.log('error:', error); // Print the error if one occurred
+        // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        // console.log(response);
+        console.log(body); // Print the HTML for the Google homepage.
+        console.log("Title: ", body["Title"]);
+        console.log("Year: ", body.Year);
+        console.log("IMDB Rating: ", body.imdbRating);
+        // console.log("Rotten Tomatoes Rating: ", body.imdbRating);
+        console.log("Country: ", body.Country);
+        console.log("Language: ", body.Language);
+        console.log("Plot: ", body.Plot);
+        console.log("Actors: ", body.Actors);
     });
 }
+
+function getRandomSong() {
+    fs.readFile("random.txt", function(err, data) {
+        if (err) throw err;
+    })
+}
+
+function getUserInput() {
+    const args = process.argv;
+    if (args.length < 3) {
+        console.log("Enter a valid command");
+        return;
+    }
+
+    const commandLookup = {
+        "my-tweets": getTwitter,
+        "spotify-this-song": getSpotify,
+        "movie-this": getOmdb
+    }
+    
+    const command = args[2];
+    const commandVal = args[3];
+
+    if (typeof commandLookup[command] !== "undefined") {
+        commandLookup[command](commandVal);
+    }
+    else if (command === "do-what-it-says") {
+        // getSpotify(getRandomSong());
+    }
+    else {
+        console.log("\"" + command  + "\" is not a valid command" );
+    }
+}
+
+getUserInput();
 
 // function getUserInput() {
 //     inquirer.prompt([
@@ -108,41 +178,3 @@ function getOmdb(movieTitle) {
 //         resolve = userInput;
 //     });
 // }
-
-function getRandomSong() {
-    fs.readFile("random.txt", function(err, data) {
-        if (err) throw err;
-    })
-}
-
-function getUserInput() {
-    const args = process.argv;
-    if (args.length < 3) {
-        console.log("Enter a valid command");
-        return;
-    }
-
-    const commandLookup = {
-        "my-tweets": getTwitter,
-        "spotify-this-song": getSpotify,
-        "movie-this": getOmdb
-    }
-    
-    const command = args[2];
-    const commandVal = args[3];
-
-    if (typeof commandLookup[command] !== "undefined") {
-        commandLookup[command](commandVal);
-    }
-    else if (command === "do-what-it-says") {
-        // getSpotify(getRandomSong());
-    }
-    else {
-        console.log("\"" + command  + "\" is not a valid command" );
-    }
-}
-
-// getTwitter();
-// getSpotify();
-// getOmdb("Mr   Nobody");
-getUserInput();
